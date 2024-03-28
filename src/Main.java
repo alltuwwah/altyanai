c634cc488263b5486d1a7e3af22f7efb23ace6c6
Altynai <>
2024-03-28 17:09:04 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import javax.swing.plaf.IconUIResource;
+import java.util.Scanner;
+
+//(Цикл while)
+//Задача №1 Количество чисел в последовательности.
+// С клавиатуры вводятся целые числа, пока не будет введено отрицательное число. Вывести на экран количество введенных чисел.
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = 0;
+        int number;
+
+        //System.out.println("Введите целые числа :");
+
+        System.out.print("Введите число: ");
+        number = scanner.nextInt();
+
+        while (number >= 0) {
+             a++;
+            System.out.print("Введите число: ");
+            number = scanner.nextInt();
+        }
+
+        System.out.println("Количество введенных чисел: " + a);
+
+}
+}
+       
+
+
+
+
